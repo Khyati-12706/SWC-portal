341a573bca0d2d7c33b68bfb36b3badd83805720
Khyati-12706 <Khyatibajaj903@gmail.com>
2025-07-03 20:55:25 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -12,16 +12,16 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // REACT_APP_FIREBASE_APP_ID=your-app-id
 // REACT_APP_FIREBASE_MEASUREMENT_ID=your-measurement-id
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBc1xo02uP1oUTkWlJ6OiOs8uaaqcC1-p0', // User-provided API key
+  authDomain: 'vit-swc-admin.firebaseapp.com',
+  projectId: 'vit-swc-admin',
+  storageBucket: 'vit-swc-admin.appspot.com',
+  messagingSenderId: '684394450806', 
+  appId: '1:684394450806:web:3cb309535b76ec16027457', 
+  measurementId: 'G-2174GVT6GH'
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
