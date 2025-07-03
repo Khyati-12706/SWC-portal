b8e61134c2e6750d3b0020c72eb68025b716d89d
Khyati-12706 <Khyatibajaj903@gmail.com>
2025-07-03 21:06:28 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -14,13 +14,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBc1xo02uP1oUTkWlJ6OiOs8uaaqcC1-p0', // User-provided API key
-  authDomain: 'vit-swc-admin.firebaseapp.com',
-  projectId: 'vit-swc-admin',
-  storageBucket: 'vit-swc-admin.appspot.com',
-  messagingSenderId: '684394450806', 
-  appId: '1:684394450806:web:3cb309535b76ec16027457', 
-  measurementId: 'G-2174GVT6GH'
+  REACT_APP_FIREBASE_API_KEY=your-api-key
+REACT_APP_FIREBASE_AUTH_DOMAIN=vit-swc-admin.firebaseapp.com
+REACT_APP_FIREBASE_PROJECT_ID=vit-swc-admin
+REACT_APP_FIREBASE_STORAGE_BUCKET=vit-swc-admin.appspot.com
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID=your-messaging-sender-id
+REACT_APP_FIREBASE_APP_ID=your-app-id
+REACT_APP_FIREBASE_MEASUREMENT_ID=your-measurement-id
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
